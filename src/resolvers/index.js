510e713d063508d8daad410e610d60cd588211d4
Felicia <87656480+felicianorman@users.noreply.github.com>
2023-01-13 12:19:20 +0100

    användare kan lägga till böcker

--- src/resolvers/index.js
+++ src/resolvers/index.js
@@ -3,23 +3,85 @@ const fsPromises = require("fs/promises");
 const { fileExists, readJsonFile } = require("../utils/fileHandling");
 const { GraphQLError } = require("graphql");
 const crypto = require("node:crypto");
+const { read } = require("fs");
 
 exports.resolvers = {
-    Query: {
-        getBookById: async (_, args) => {
-            const bookId = args.bookId;
+  Query: {
+    getBookById: async (_, args) => {
+      const bookId = args.bookId;
 
-            const bookFilePath = path.join(__dirname, `./../data/projects/${bookId}.json`);
+      const bookFilePath = path.join(
+        __dirname,
+        `./../data/projects/${bookId}.json`
+      );
 
-            const bookExists = await fileExists(bookFilePath);
-            if(!bookExists) return new GraphQLError('Den boken existerar inte');
+      const bookExists = await fileExists(bookFilePath);
+      if (!bookExists) return new GraphQLError("Den boken existerar inte");
 
-            const bookData = await fsPromises.readFile(bookFilePath, {
-                encoding: "utf-8"
-            });
+      const bookData = await fsPromises.readFile(bookFilePath, {
+        encoding: "utf-8",
+      });
 
-            const data = JSON.parse(bookData);
-            return data;
+      const data = JSON.parse(bookData);
+      return data;
+    },
+    getAllBooks: async (_, args) => {
+      const booksDirectory = path.join(__dirname, `../data/projects/`);
+
+      const books = await fsPromises.readdir(booksDirectory);
+      const promises = [];
+      books.forEach((fileName) => {
+        const filePath = path.join(booksDirectory, fileName);
+        promises.push(readJsonFile(filePath));
+      });
+
+      const bookData = await Promise.all(promises);
+      return bookData;
+    },
+  },
+  Mutation: {
+    createBook: async (_, args) => {
+      //Verify name
+      if (args.name.lenght === 0)
+        return new GraphQLError("Bok namnet måste vara minst 1 bokstav");
+
+      //Unikt id
+      const newBook = {
+        id: crypto.randomUUID(),
+        name: args.name,
+        author: args.author,
+        genre: args.genre,
+        description: args.description || "",
+      };
+
+      let filePath = path.join(
+        __dirname,
+        `../data/projects/${newBook.id}.json`
+      );
+
+      let idExists = true;
+
+      //Kontrollerar om boken existerar
+      //Om den existerar, generera ett randomiserat ID
+      while (idExists) {
+        const exists = await fileExists(filePath);
+        console.log(exists, newBook.id);
+
+        if (exists) {
+          newBook.id = crypto.randomUUID();
+          filePath = path.join(
+            __dirname,
+            `../data/projects/${newBook.id}.json`
+          );
         }
-    }
-}
+        idExists = exists;
+      }
+
+      //Skapar fil för nya boken
+      await fsPromises.writeFile(filePath, JSON.stringify(newBook));
+
+      //Returnerar vår respons
+      return newBook;
+    },
+  },
+};
