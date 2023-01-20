3578b662bf225933a249385d10133f4fdbb04c7c
Felicia <87656480+felicianorman@users.noreply.github.com>
2023-01-20 16:41:31 +0100

    ny filstruktur

--- src/resolvers/index.js
+++ src/resolvers/index.js
@@ -1,115 +1,29 @@
 const path = require("path");
 const fsPromises = require("fs/promises");
-const { fileExists, readJsonFile } = require("../utils/fileHandling");
-const { GraphQLError } = require("graphql");
-const crypto = require("node:crypto");
-const { read } = require("fs");
+const {
+  fileExists,
+  readJsonFile,
+  deleteFile,
+  getDirectoryFileNames,
+} = require("../utils/fileHandling");
+const { GraphQLError, printType } = require("graphql");
+const crypto = require("crypto");
+const axios = require("axios").default;
 
 exports.resolvers = {
   Query: {
-    getBookById: async (_, args) => {
-      const bookId = args.bookId;
+    getAllBooks: async (_) => {
+      let bookData = [];
 
-      const bookFilePath = path.join(
-        __dirname,
-        `./../data/projects/${bookId}.json`
-      );
-
-      const bookExists = await fileExists(bookFilePath);
-      if (!bookExists) return new GraphQLError("Den boken existerar inte");
-
-      const bookData = await fsPromises.readFile(bookFilePath, {
-        encoding: "utf-8",
-      });
-
-      const data = JSON.parse(bookData);
-      return data;
-    },
-    getAllBooks: async (_, args) => {
-      const booksDirectory = path.join(__dirname, `../data/projects/`);
-
-      const books = await fsPromises.readdir(booksDirectory);
-      const promises = [];
-      books.forEach((fileName) => {
-        const filePath = path.join(booksDirectory, fileName);
-        promises.push(readJsonFile(filePath));
-      });
-
-      const bookData = await Promise.all(promises);
+      try {
+        const response = await axios.get(process.env.BOOK_URI);
+        console.log(response.data);
+        bookData = response.data;
+      } catch (error) {
+        console.log(error);
+        return new GraphQLError("Något gick fel");
+      }
       return bookData;
     },
-  },
-  Mutation: {
-    createBook: async (_, args) => {
-      //Verify name
-      if (args.name.lenght === 0)
-        return new GraphQLError("Bok namnet måste vara minst 1 bokstav");
-
-      //Unikt id
-      const newBook = {
-        id: crypto.randomUUID(),
-        name: args.name,
-        author: args.author,
-        genre: args.genre,
-        description: args.description || "",
-        finished: args.finished
-      };
-
-      let filePath = path.join(
-        __dirname,
-        `../data/projects/${newBook.id}.json`
-      );
-
-      let idExists = true;
-
-      //Kontrollerar om boken existerar
-      //Om den existerar, generera ett randomiserat ID
-      while (idExists) {
-        const exists = await fileExists(filePath);
-        console.log(exists, newBook.id);
-
-        if (exists) {
-          newBook.id = crypto.randomUUID();
-          filePath = path.join(
-            __dirname,
-            `../data/projects/${newBook.id}.json`
-          );
-        }
-        idExists = exists;
-      }
-
-      //Skapar fil för nya boken
-      await fsPromises.writeFile(filePath, JSON.stringify(newBook));
-
-      //Returnerar vår respons
-      return newBook;
-    },
-    updateBook: async (_, args) => {
-        //Hämtar parametrar
-        const { id, finished } = args;
-
-        //Skapar filepath till boken
-        let filePath = path.join(__dirname, `../data/projects/${id}.json`);
-
-        //Kollar om boken vi vill ändra existerar
-        const bookExists = await fileExists(filePath);
-        if(!bookExists) return new GraphQLError("Den boken existerar inte");
-
-        //Skapar updateBook
-        const updateBook = {
-            id,
-            boolean
-        }
-
-        //Skriver över den gamla filen med den nya
-        await fsPromises.writeFile(filePath, JSON.stringify(updateBook));
-
-        //Returnerar uppdaterade objektet
-        return {
-            id: id,
-            finished: true
-        }
-
-    }
   },
 };
--- src/server.js
+++ src/server.js
@@ -4,22 +4,41 @@ const { resolvers } = require("./resolvers");
 const { loadFiles } = require("@graphql-tools/load-files");
 const { makeExecutableSchema } = require("@graphql-tools/schema");
 const path = require("path");
-const { startStandaloneServer } = require("@apollo/server/standalone");
-const { type } = require("os");
+const { expressMiddleware } = require("@apollo/server/express4");
+const express = require("express");
+
+const app = express();
+app.use(express.json());
+
+app.use(express.static(path.join(__dirname, "public")));
+app.use(express.static(path.join(__dirname, "views")));
+
+const port = process.env.PORT || 5000;
 
 async function run() {
   try {
+    //loadFiles läser av vår schema.graphQL och lagrar i variabeln typeDefs
     const typeDefs = await loadFiles(path.join(__dirname, "./schema.graphql"));
+
+    //här tar vi våra resolvers (all logik) och schemagraphQL och gör om till ett
+    //schema som apollo kan använda för att starta sin server
     const schema = makeExecutableSchema({
       typeDefs: typeDefs,
       resolvers: resolvers,
     });
 
+    //Skapar vår server
     const server = new ApolloServer({ schema: schema });
-    const res = await startStandaloneServer(server);
-    console.log(`Server ready at ${res.url}`);
+
+    //startar vår server
+    await server.start();
+    app.use("/graphql", expressMiddleware(server));
+
+    app.listen(port, () => {
+      console.log(`Server ready at http://localhost:${port}`);
+    });
   } catch (error) {
-    console.log(error)
+    console.log(error);
   }
 }
 
